d2202a9b4955acb8ca8c1c6d99341949372c7e84
“NayanPal005” <nayancse005@gmail.com>
2018-06-26 01:57:03 +0600

    Blog based on specifice category display in the front using join query successfully

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -94,11 +94,11 @@ class WelcomeController extends Controller
 
     public function categorywise_blog($category_id){
 
-        echo "This Category id is:".$category_id;
+      //  echo "This Category id is:".$category_id;
 
 
         $categorywise_blog= DB::table('tbl_category')
-//
+
 //         //$categorywise_blog= DB::table('tbl_blog')
 //
                 ->join('tbl_blog','tbl_category.category_id','=','tbl_blog.category_id')
@@ -107,13 +107,16 @@ class WelcomeController extends Controller
                //  ->where('tbl_blog.category_id','>',$category_id) eta dile ei sign(>) ti category_id joto tar ek ghor samner category_id er blog dekhabe :)
 
                  ->where('tbl_blog.category_id',$category_id)
-//
+                //must tbl_blog.category_id or tbl_category.category_id ditei hbe in laravel
                  ->select('tbl_category.*','tbl_blog.*')
                  ->get();
+        $categorywise_blog_result=view('pages.categorywise_blog')->with('categorywise_blog',$categorywise_blog);
 
-     echo '<pre>';
-      print_r($categorywise_blog);
-     exit();
+        return view('master')->with('main_content',$categorywise_blog_result);
+
+//     echo '<pre>';
+//      print_r($categorywise_blog);
+//     exit();
 
     }
     public function search_blog(Request $request){
--- resources/views/pages/categorywise_blog.blade.php
+++ resources/views/pages/categorywise_blog.blade.php
@@ -2,9 +2,9 @@
 @section('main_content')
 
     <?php
-    //      echo '<pre>';
-    //      print_r($blog_infoupdate);
-    //    exit();
+//       echo '<pre>';
+//        print_r($categorywise_blog);
+//     exit();
     ?>
 
     <div id="templatemo_main_wrapper">
@@ -14,7 +14,7 @@
                 <div id="templatemo_content">
 
 
-
+                    <?php foreach ($categorywise_blog as $categorywise_blog): ?>
                     <div class="post_section">
 
                         <div class="post_date">
@@ -22,20 +22,22 @@
                         </div>
                         <div class="post_content">
 
-                            <h2><a href=""><?php echo $blog_infoupdate->blog_title?></a></h2>
+                            <h2><a href=""><?php echo $categorywise_blog ->blog_title?></a></h2>
 
-                            <strong>Author:</strong><?php echo $blog_infoupdate->author_name; ?> <strong>Category:</strong> <a href="#"></a>
+                            <strong>Author:</strong><?php echo $categorywise_blog->author_name; ?> <strong>Category:</strong> <a href="#"></a>
 
 
-                            <a href="http://www.templatemo.com/page/1" target="_parent"><img width="100%" class="img-circle" src="{{ URL::asset($blog_infoupdate->blog_image) }}"></a>
+                            <a href="http://www.templatemo.com/page/1" target="_parent"><img width="100%" class="img-circle" src="{{ URL::asset($categorywise_blog->blog_image) }}"></a>
 
-                            <p><?php echo $blog_infoupdate->blog_short_description; ?></p>
-                            <p> <?php echo $blog_infoupdate->blog_long_description; ?></p>
+                            <p><?php echo $categorywise_blog->blog_short_description; ?></p>
+                            <p> <?php echo $categorywise_blog->blog_long_description; ?></p>
 
                         </div>
 
 
                     </div>
+
+                    <?php endforeach; ?>
 
 
                 </div>
@@ -45,47 +47,6 @@
 
                     <div id="templatemo_sidebar">
 
-                        <h4>Categories</h4>
-                        <?php
-
-                        //   print_r($all_category);
-                        foreach ($all_published_category as $all_published_category):
-
-                        ?>
-
-                        <ul class="templatemo_list">
-                            <li><a href="{{\Illuminate\Support\Facades\URL::to('/categorywise_blog/'.$all_published_category->category_id)}}"><?php echo  $all_published_category->category_name ?></a></li>
-
-                        </ul>
-                        <?php endforeach; ?>
-
-                        <div class="cleaner_h40"></div>
-
-                        <h4>Latest Blog</h4>
-                        <?php
-                        $latest_blog = DB::table('tbl_blog')
-                            ->where('publication_status',1)
-                            ->orderby('blog_id','desc')
-                            ->limit(3)
-                            ->get();
-
-
-                        ?>
-
-
-                        <?php foreach ($latest_blog as $latest_blogs): ?>
-                        <ul class="templatemo_list">
-                            <li><a href="{{\Illuminate\Support\Facades\URL::to('/blog-details/'.$latest_blogs->blog_id)}}"><?php echo $latest_blogs->blog_title;?></a></li>
-
-                        </ul>
-                        <?php endforeach; ?>
-
-
-                        <div id="ads">
-                            <a href="#"><img src="{{asset('public/front_end_asset/images/templatemo_200x100_banner.jpg')}}" alt="banner 1" /></a>
-
-                            <a href="#"><img src="{{asset('public/front_end_asset/images/templatemo_200x100_banner.jpg')}}" alt="banner 2" /></a>
-                        </div>
 
                     </div>
 
